a8720bfc29eb1c43f0e9a6676353e9a45556696e
daqichen <daqichen@live.unc.edu>
2022-11-08 10:03:52 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+import minimist from 'minimist';
+import express, { Router } from "express";
+import { roll } from './lib/roll.js';
+
+const args = minimist(process.argv.slice(2));
+console.log(args)
+const port = args.port? args.port:5000;
+
+const app = express();
+const router = Router();
+
+router.get('/', function (req, res) {
+    res.send('200 OK');
+  })
+
+router.get('/roll', function (req, res) {
+    console.log(req.body);
+    const sides = req.body.sides? req.body.sides:6;
+    const dice = req.body.dice? req.body.dice:2;
+    const rolls = req.body.rolls? req.body.rolls:1;
+    res.send(JSON.stringify(roll(sides, dice, rolls)));
+  })
+
+router.get('/roll/:sides', function (req, res) {
+    console.log(req.body);
+    const sides = req.params.sides;
+    const dice = 2;
+    const rolls = 1;
+    res.send(JSON.stringify(roll(sides, dice, rolls)));
+  })
+
+router.get('/roll/:sides/:dice', function (req, res) {
+    console.log(req.body);
+    const sides = req.params.sides;
+    const dice = req.params.dice;
+    const rolls = 1;
+    res.send(JSON.stringify(roll(sides, dice, rolls)));
+  })
+
+router.get('/roll/:sides/:dice/:rolls', function (req, res) {
+    console.log(req.body);
+    const sides = req.params.sides;
+    const dice = req.params.dice;
+    const rolls = req.params.rolls;
+    res.send(JSON.stringify(roll(sides, dice, rolls)));
+  })
+
+app.use('/app', router);
+app.use(function (req, res) {
+    res.send('404 NOT FOUND');
+})
+
+app.listen(port)
+console.log('Server listening on port ' + port);
